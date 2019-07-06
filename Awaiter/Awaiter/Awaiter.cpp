c4c0405f4f8a6d73ee8839536f253507c7291050
CuriouslyRecurringThoughts <curiouslyrecurringthoughts@gmail.com>
2019-07-06 17:27:40 +0200

    std::future works, task on the other hand is not blocking.

--- Awaiter/Awaiter/Awaiter.cpp
+++ Awaiter/Awaiter/Awaiter.cpp
@@ -1,17 +1,14 @@
 // GeneratorCoRoutines.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <mutex>
-#include <string>
 #include <thread>
 #include <future>
 #include <iostream>
-#include <optional>
 #include <experimental/coroutine>
 
 class Waiter {
 public:
-
+	//Neede by Awaitable concept
 	bool await_ready();
 	void await_suspend(std::experimental::coroutine_handle<>);
 	void await_resume();
@@ -20,12 +17,10 @@ public:
 	void SetReady();
 private:
 	std::experimental::coroutine_handle<> m_handle;
-	bool m_isReady{ false };
 };
 
 void Waiter::SetReady()
 {
-	m_isReady = true;
 	std::cout << "SET READY!\n";
 	if (m_handle)
 		m_handle.resume();
@@ -33,7 +28,7 @@ void Waiter::SetReady()
 
 bool Waiter::await_ready()
 {
-	return m_isReady;
+	return false;
 }
 
 void Waiter::await_suspend(std::experimental::coroutine_handle<> handle)
@@ -46,7 +41,9 @@ void Waiter::await_resume()
 	std::cout << "I am DONE Waiting! \n";
 }
 
+using task = std::future<void>;
 
+/*
 struct task {
 	struct promise_type;
 
@@ -67,18 +64,27 @@ struct task {
 private:
 	handle coro;
 };
+*/
 
-std::future<void> func() {
+Waiter waiter;
+
+task func() {
 
 	std::cout << "Func()\n";
 
-	Waiter waiter;
+	std::cout << "about to await!\n";
 
-	std::cout << "Waiter created\n";
+	co_await waiter;
 
+	std::cout << "co await passed!\n";
+
+}
+
+int main()
+{
 	std::thread t
 	{
-		[&waiter]()
+		[]()
 		{
 			std::cout << "About to set sleep\n";
 			std::this_thread::sleep_for(std::chrono::duration<int>(3));
@@ -87,21 +93,13 @@ std::future<void> func() {
 		}
 	};
 
-	t.detach();
 
-	std::cout << "about to await!\n";
-
-	co_await waiter;
-
-	std::cout << "co await passed!\n";
-	std::cout << "after join!\n";
-}
-
-int main()
-{
 	std::cout << "About to func()\n";
 	auto a = func();
-	std::this_thread::sleep_for(std::chrono::duration<int>(4));
 	a.get();
+
+	t.join();
+	std::cout << "after join!\n";
+
 }
 
