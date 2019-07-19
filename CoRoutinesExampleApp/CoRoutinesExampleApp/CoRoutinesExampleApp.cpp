7e93850d0c532c096f264dea477cb5283eadf0ab
CuriouslyRecurringThoughts <curiouslyrecurringthoughts@gmail.com>
2019-07-19 22:22:32 +0200

    CoRoutines CoAwait example

--- CoRoutinesExampleApp/CoRoutinesExampleApp/CoRoutinesExampleApp.cpp
+++ CoRoutinesExampleApp/CoRoutinesExampleApp/CoRoutinesExampleApp.cpp
@@ -1,66 +1,104 @@
 // CoRoutinesExampleApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <thread>
 #include <iostream>
 #include <string>
 #include <future>
+#include <queue>
+#include <utility>
 #include <experimental/coroutine>
-#include <experimental/generator>
 
-#include "WorkerQueue.h"
 
-template<typename T>
-class QueueAwaiter {
+class Worker {
+
+public:
+
+	static Worker& getInstance() {
+		static Worker worker;
+		return worker;
+	}
+
+	using coroHandle = std::experimental::coroutine_handle<>;
+
+	void suspend(coroHandle handle, int inputData) {
+		m_suspendedCoros.push({handle, inputData});
+	}
+
+	int veryComplexDataGeneration(int inputData) { return std::rand() % 10 + inputData; }
+
+	void run() {
+		unsigned int i = 0;
+		while (!m_suspendedCoros.empty()) {
+			auto [coro, inputData]= m_suspendedCoros.front();
+			m_suspendedCoros.pop();
+			outputData = veryComplexDataGeneration(inputData);
+			coro.resume();
+		}
+	}
+
+	int getData() const { return outputData; }
+
+private:
+
+	std::queue<std::pair<coroHandle, int>> m_suspendedCoros;
+	int outputData{ -1 };
+};
+
+class Awaiter {
 public:
 	
-	QueueAwaiter(WorkerQueue<T>& queue) : m_queue{ queue } { }
+	Awaiter(int data) : data(data) {}
 
 	//Needed by Awaiter concept
 	bool await_ready()
 	{
-		return !m_queue.empty();
+		return false;
 	}
 
 	void await_suspend(std::experimental::coroutine_handle<> handle)
 	{
-		m_queue.wakeMeUp(handle);
+		Worker::getInstance().suspend(handle, data);
 	}
 
-	T await_resume()
+	int await_resume()
 	{
-		return  m_queue.pop();
+		return Worker::getInstance().getData();
 	}
 
 private:
-	WorkerQueue<T>& m_queue;
+	int data;
 };
 
-std::future<void> consumer(WorkerQueue<std::string>& queue) {
-	while (true) {
-		QueueAwaiter<std::string> aw{ queue };
-		std::string result = co_await aw;
-		std::cout << "Returned " << result << '\n';
+struct noisy {
+
+	noisy(const std::string& name) : m_name{name} { std::cout << "noisy for coroutine " << m_name << " created\n"; }
+	~noisy() { { std::cout << "noisy for coroutine " << m_name << " destroyed\n"; } }
+
+	std::string m_name;
+};
+
+std::future<int> coRoutine(std::string name){
+
+	noisy ns{ name };
+
+	int sum = 0;
+	while (sum < 20) {
+		int input_data = rand() % 5;
+		const auto s = co_await Awaiter{input_data};
+		std::cout << name << " received " << s << '\n';
+		sum += s;	
 	}
+	co_return sum;
 }
 
+
 int main()
-{
-	WorkerQueue <std::string> queue;
+{	
+	auto f1 = coRoutine("c1");
+	auto f2 = coRoutine("c2");
+	auto f3 = coRoutine("c3");
+	
+	Worker::getInstance().run();
 
-	std::thread producer{
-		[&queue]() {
-			unsigned int i = 0;
-			while(true){
-				std::string s = "HELLO" + std::to_string(i++);
-				std::cout << "Produced " << s << '\n';
-				queue.push(std::move(s));
-			}
-		}
-	};
-
-	producer.detach();
-
-	auto f = consumer(queue);
-	f.get();
+	std::cout << f1.get() << " " << f2.get() << " " << f3.get() << '\n';
 }
